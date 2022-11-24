773b3f52071acad970092d256e5cb8ab5450b6d2
William Nathan <38376527+Xenonition@users.noreply.github.com>
2022-11-23 17:07:10 -0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import brickschema
+
+
+# In[7]:
+
+
+g = brickschema.Graph(load_brick=True)
+g.load_file("test/testdata/ebu3b_brick.ttl")
+
+
+# In[ ]:
+
+
+
+
